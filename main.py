245cd183a68c155af4e49e7b0fd99d7755645624
Ali Mirzaei <a.mirzaei69@gmail.com>
2017-09-15 03:31:11 +0430

    adds AAE

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ encoded_dim = 2
 
 encoder = Sequential()
 encoder.add(Flatten(input_shape=img_shape))
-encoder.add(Dense(100, activation='relu'))
-encoder.add(Dense(100, activation='relu'))
+encoder.add(Dense(1000, activation='relu'))
+encoder.add(Dense(1000, activation='relu'))
 encoder.add(Dense(encoded_dim, activation='relu'))
 
 encoder.summary()
@@ -30,8 +30,8 @@ encoded_repr = encoder(img)
 # Decoder
 decoder = Sequential()
 
-decoder.add(Dense(100,activation='relu', input_dim=encoded_dim))
-decoder.add(Dense(100,activation='relu'))
+decoder.add(Dense(1000,activation='relu', input_dim=encoded_dim))
+decoder.add(Dense(1000,activation='relu'))
 decoder.add(Dense(np.prod(img_shape), activation='sigmoid'))
 decoder.add(Reshape(img_shape))
 
@@ -39,12 +39,76 @@ decoder.summary()
 
 gen_img = decoder(encoded_repr)
 
-generator = Model(img, gen_img)
+autoencoder = Model(img, [encoded_repr, gen_img])
 
-generator.compile(optimizer='adadelta', loss='binary_crossentropy')
 
-generator.fit(x_train, x_train, epochs=10, batch_size=100)
-decoded_imgs = generator.predict(x_test)
+# Discriminator
+discriminator = Sequential()
+discriminator.add(Dense(1000, activation='relu', input_dim=encoded_dim))
+discriminator.add(Dense(1000, activation='relu'))
+discriminator.add(Dense(1, activation='sigmoid'))
+discriminator.summary()
+valid = discriminator(encoded_repr)
+adversarial_autoencoder = Model(img, [gen_img, valid])
+
+adversarial_autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+discriminator.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+batch_size = 256
+epochs = 500
+half_batch = int(batch_size / 2)
+
+for epoch in range(epochs):
+    # ---------------------
+    #  Train Discriminator
+    # ---------------------
+
+    # Select a random half batch of images
+    idx = np.random.randint(0, x_train.shape[0], half_batch)
+    imgs = x_train[idx]
+
+    # Generate a half batch of new images
+    latent_fake, gen_imgs = autoencoder.predict(imgs)
+    latent_real = np.random.normal(size=(half_batch, encoded_dim))
+
+    valid = np.ones((half_batch, 1))
+    fake = np.zeros((half_batch, 1))
+
+    # Train the discriminator
+    d_loss_real = discriminator.train_on_batch(latent_real, valid)
+    d_loss_fake = discriminator.train_on_batch(latent_fake, fake)
+    d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+
+    # ---------------------
+    #  Train Generator
+    # ---------------------
+
+    # Select a random half batch of images
+    idx = np.random.randint(0, x_train.shape[0], batch_size)
+    imgs = x_train[idx]
+
+    # Generator wants the discriminator to label the generated representations as valid
+    valid_y = np.ones((batch_size, 1))
+
+    # Train the generator
+    g_loss = adversarial_autoencoder.train_on_batch(imgs, [imgs, valid_y])
+
+    # Plot the progress
+    print ("%d [D loss: %f] [G loss: %f]" % (epoch, d_loss, g_loss[0]))
+
+#    # If at save interval => save generated image samples
+#    if epoch % save_interval == 0:
+#        # Select a random half batch of images
+#        idx = np.random.randint(0, X_train.shape[0], 25)
+#        imgs = X_train[idx]
+#        self.save_imgs(epoch, imgs)
+#
+#autoencoder.fit(x_train, x_train, epochs=10, batch_size=256)
+
+decoded_imgs = autoencoder.predict(x_test)
+
+
 n = 10  # how many digits we will display
 plt.figure(figsize=(20, 4))
 for i in range(n):
