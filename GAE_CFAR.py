48188845d017492f5a8204eca2b792e9a35dfcf1
Ali Mirzaei <a.mirzaei69@gmail.com>
2017-11-13 23:53:33 +0330

    adds CFAR implemetation

--- GAE_CFAR.py
+++ GAE_CFAR.py
@@ -6,7 +6,7 @@
 
 
 from keras.models import Sequential, Model
-from keras.layers import Dense, Input, Flatten, Reshape
+from keras.layers import Dense, Input, Flatten, Reshape, Conv2D, Conv2DTranspose
 from keras.datasets import mnist, cifar10
 from keras.optimizers import Adam
 import numpy as np
@@ -16,12 +16,12 @@ from mpl_toolkits.mplot3d import Axes3D
 import helpers
 from sklearn.model_selection import GridSearchCV
 import keras
-
+from tqdm import tqdm
 
 class GAE():
     def __init__(self, img_shape=(28, 28), encoded_dim=2):
         self.encoded_dim = encoded_dim
-        self.optimizer = Adam(0.000001)
+        self.optimizer = Adam(0.001)
         self._initAndCompileFullModel(img_shape, encoded_dim)
         self.img_shape = img_shape
 
@@ -34,10 +34,11 @@ class GAE():
             A sequential keras model
         """
         encoder = Sequential()
-        encoder.add(Flatten(input_shape=img_shape))
-        encoder.add(Dense(1000, activation='relu'))
-        encoder.add(Dense(1000, activation='relu'))
-        encoder.add(Dense(encoded_dim))
+        #encoder.add(Flatten(input_shape=img_shape))
+        encoder.add(Conv2D(32, (5, 5), activation='tanh', input_shape=img_shape))
+        encoder.add(Flatten())
+        #encoder.add(Dense(100, activation='tanh'))
+        encoder.add(Dense(encoded_dim, activation='tanh'))
         encoder.summary()
         return encoder
 
@@ -50,9 +51,10 @@ class GAE():
             A sequential keras model
         """
         decoder = Sequential()
-        decoder.add(Dense(1000, activation='relu', input_dim=encoded_dim))
-        decoder.add(Dense(1000, activation='relu'))
-        decoder.add(Dense(np.prod(img_shape), activation='sigmoid'))
+        #decoder.add(Dense(100, activation='tanh', input_dim=encoded_dim))
+        decoder.add(Dense((img_shape[0] - 2) * (img_shape[1] - 2) * 3, activation='tanh', input_dim=encoded_dim))
+        decoder.add(Reshape(((img_shape[0] - 2), (img_shape[1] - 2), 3)))
+        decoder.add(Conv2DTranspose(3, (3,3), activation='sigmoid'))
         decoder.add(Reshape(img_shape))
         decoder.summary()
         return decoder
@@ -82,56 +84,120 @@ class GAE():
         plt.close(fig)
 
     def train(self, x_train, batch_size=32, epochs=5):
-        self.autoencoder.fit(x_train, x_train, batch_size=batch_size,
-                             epochs=epochs, 
-                             callbacks=[keras.callbacks.ModelCheckpoint('weights.{epoch:02d}.hdf5', 
-                                                                       verbose=0, 
-                                                                       save_best_only=False, 
-                                                                       save_weights_only=False, 
-                                                                       mode='auto', 
-                                                                       period=1)])
-        codes = self.encoder.predict(x_train)
+        self.autoencoder.load_weights('15619.hdf5')
+        l=0
+        tq = tqdm(range(len(x_train)*epochs/batch_size))
+        for i in tq:
+            tq.set_description("MSE Loss (%f)" % l)
+            idx= np.random.randint(0, len(x_train), batch_size)
+            x_batch = x_train[idx]
+            x_out = copy.copy(x_batch)
+            mask = np.random.choice([0, 1], size=(batch_size, 32, 32, 1), p=[1./10, 9./10])
+            mask = np.repeat(mask, 3, 3)
+            mm = np.ma.masked_array(x_batch, mask= mask)
+            x_batch[mm.mask]=0
+            l = self.autoencoder.train_on_batch(x_batch, x_out)
+            if(i%(len(x_train)/batch_size)==(len(x_train)/batch_size)-1):
+                self.autoencoder.save_weights(str(i)+'.hdf5')
+#                                 callbacks=[keras.callbacks.ModelCheckpoint('weights_cifar.{epoch:02d}.hdf5', 
+#                                                                           verbose=0, 
+#                                                                           save_best_only=False, 
+#                                                                           save_weights_only=False, 
+#                                                                           mode='auto', 
+#                                                                           period=1)])
+#        codes = self.encoder.predict(x_train)
 #        params = {'bandwidth': [3.16]}#np.logspace(0, 2, 5)}
 #        grid = GridSearchCV(KernelDensity(), params, n_jobs=4)
 #        grid.fit(codes)
 #        print grid.best_params_
 #        self.kde = grid.best_estimator_
-        self.kde = KernelDensity(kernel='gaussian', bandwidth=.2).fit(codes)
+#        self.kde = KernelDensity(kernel='gaussian', bandwidth=.2).fit(codes)
 
     def generate(self, n = 10000):
         codes = self.kde.sample(n)
         images = self.decoder.predict(codes)
         return images
 
-    def generateAndPlot(self, x_train, n = 10):
+    def generateAndPlot(self, x_test):
         fig = plt.figure(figsize=[20, 20])
-        images = self.generate(n*n)
+        images = x_test
         index = 1
-        for image in images:
+        reconstructed_image = self.autoencoder.predict(x_test)
+        for image,r in zip(images, reconstructed_image):
             image = image.reshape(self.img_shape)
-            ax = fig.add_subplot(n, n+1, index)
+            ax = fig.add_subplot(len(x_test), 2, index)
             index=index+1
             ax.set_axis_off()
             ax.imshow(image, cmap="gray")
-            if((index)%(n+1) == 0):
-                nearest = helpers.findNearest(x_train, image)
-                ax = fig.add_subplot(n, n+1, index)
-                index= index+1
-                ax.imshow(nearest, cmap="gray")
-        fig.savefig("generated.png")
+            ax = fig.add_subplot(len(x_test), 2, index)
+            index=index+1
+            ax.set_axis_off()
+            ax.imshow(r, cmap="gray")
+            
+        fig.savefig("cfar10_generated.png")
         plt.show()
 
     def meanLogLikelihood(self, x_test):
         KernelDensity(kernel='gaussian', bandwidth=0.2).fit(codes)
+
+
+def plotResults(ann, x_test, key_numbers = 10):
+    fig = plt.figure(figsize=(10, 10*len(x_test)/4))
+    for index, x in enumerate(x_test):
+        print('Processing Image ', index)
+        selected_pixels = []
+        best_partial_x = np.zeros(x.shape)
+        for p in range(key_numbers):
+            errors = []
+            partial_images = []
+            for pixel in range(32*32):
+                partial_x = copy.copy(best_partial_x)
+                partial_x[pixel/32, pixel%32,:] = x[pixel/32, pixel%32,:]
+                partial_images.append(copy.copy(partial_x))
+            partial_images= np.array(partial_images)
+            y = ann.autoencoder.predict(partial_images)
+            errors = np.sum(np.abs(y-x), axis=(1,2,3))
+            best_pixel = np.argmin(errors)
+            best_partial_x[best_pixel/32, best_pixel%32,:] = x[best_pixel/32, best_pixel%32,:]
+            selected_pixels.append(best_pixel)
+        partial_x = np.zeros(x.shape)
+        mask = np.zeros(x.shape)
+        for pixel in selected_pixels:
+            partial_x[pixel/32, pixel%32,:] = x[pixel/32, pixel%32,:]
+            mask[pixel/32, pixel%32] = 1
+        y = ann.autoencoder.predict(partial_x.reshape(1,32,32,3))
+        ax = fig.add_subplot(len(x_test),4,index*4+1)
+        ax.imshow(x)
+        ax = fig.add_subplot(len(x_test),4,index*4+2)
+        ax.imshow(mask)
+        ax = fig.add_subplot(len(x_test),4,index*4+3)
+        ax.imshow(partial_x)
+        ax = fig.add_subplot(len(x_test),4,index*4+4)
+        ax.imshow(y[0])
+        plt.show()
+    fig.savefig(str(key_numbers)+'cifar3.jpg')
+
+
 if __name__ == '__main__':
     # Load MNIST dataset
+    import copy
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
     x_train = x_train.astype(np.float32) / 255.
     x_test = x_test.astype(np.float32) / 255.
-    ann = GAE(img_shape = x_train[0].shape, encoded_dim=15)
-    ann.autoencoder.load_weights('weights.03.hdf5')
-    ann.train(x_train, epochs=5)
-    ann.generateAndPlot(x_train)
+#    x_out = copy.copy(x_train)
+#    x_in = []
+#    for x in x_train:
+#        mask = np.random.choice([0, 1], size=(32, 32), p=[1./10, 9./10])
+#        mask = np.dstack([mask, mask, mask])
+#        mm = np.ma.masked_array(x, mask= mask)
+#        x[mm.mask]=0
+#        x_in.append(x)
+#    x_in = np.array(x_in)
+    ann = GAE(img_shape = x_train[0].shape, encoded_dim=100)
+    #ann.autoencoder.load_weights('weights.03.hdf5')
+    ann.train(x_train, epochs=0)
+    #ann.generateAndPlot(x_in[0:10])
+    plotResults(ann, x_test[np.random.randint(0, len(x_test), 5)],100)
     #generated = ann.generate(10000)
     #L = helpers.approximateLogLiklihood(generated, x_test, searchSpace=[.1])
     #print L
